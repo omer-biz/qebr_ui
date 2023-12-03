e58585eb6751a30f5c5b9da9ea1fabc3b928e89d
omer <omerabdi@protonmail.com>
2023-12-03 21:38:09 +0300

    feat: view details about a deceased

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, input, text)
-import Html.Attributes exposing (placeholder)
+import Html exposing (Html, button, div, img, input, span, text)
+import Html.Attributes exposing (placeholder, src, value, width)
 import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode exposing (Decoder, fail, field, float, int, list, nullable, string, succeed)
@@ -23,6 +23,11 @@ qebrUrl =
         Debug.todo "when will you be hosting?"
 
 
+logoPath : String
+logoPath =
+    "static/qebr_logo.png"
+
+
 type alias Model =
     { serchQuery : String
     , page : Page
@@ -32,6 +37,7 @@ type alias Model =
 type Page
     = NotFound
     | SearchResult (List Deceased)
+    | DetailView Deceased
     | Start
 
 
@@ -68,37 +74,60 @@ type alias QebrResponse =
 type Msg
     = SearchField String
     | Search
+    | ViewDetailDeceased Deceased
     | GotQebrResponse (Result Http.Error QebrResponse)
+    | GotAfochaResponse (Result Http.Error String)
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    div []
-        [ input [ onInput SearchField, placeholder "Search" ] [ text model.serchQuery ]
+    { title = "Qebr"
+    , body =
+        [ div [] <|
+            viewSearchField model.serchQuery
+                :: (case model.page of
+                        Start ->
+                            []
+
+                        SearchResult result ->
+                            List.map viewDeceased result
+
+                        DetailView deceased ->
+                            [ viewDetailDeceased deceased ]
+
+                        NotFound ->
+                            []
+                   )
+        ]
+    }
+
+
+viewSearchField : String -> Html Msg
+viewSearchField searchQuery =
+    span []
+        [ img [ src <| qebrUrl ++ logoPath, width 300 ] []
+        , span [] []
+        , input [ onInput SearchField, placeholder "Search", value searchQuery ] []
         , button [ onClick Search ]
             [ text "Search" ]
-        , viewPage model.page
         ]
-
-
-viewPage : Page -> Html Msg
-viewPage page =
-    case page of
-        Start ->
-            div []
-                []
-
-        SearchResult result ->
-            div [] (List.map viewDeceased result)
-
-        NotFound ->
-            div [] []
 
 
 viewDeceased : Deceased -> Html Msg
 viewDeceased deceased =
     div []
         [ text deceased.fullName
+        , text " \n "
+        , text deceased.afocha
+        , button [ onClick (ViewDetailDeceased deceased) ] [ text "More" ]
+        ]
+
+
+viewDetailDeceased : Deceased -> Html Msg
+viewDetailDeceased deceased =
+    div []
+        [ text deceased.fullName
+        , text deceased.afocha
         ]
 
 
@@ -116,6 +145,19 @@ update msg model =
         Search ->
             ( model, searchQebr model.serchQuery )
 
+        ViewDetailDeceased deceased ->
+            ( { model | page = DetailView deceased }, fetchAfocha deceased.afocha )
+
+        GotAfochaResponse (Ok response) ->
+            ( updateAfochPage model response, Cmd.none )
+
+        GotAfochaResponse (Err err) ->
+            let
+                _ =
+                    Debug.log "err " err
+            in
+            ( model, Cmd.none )
+
         GotQebrResponse (Ok response) ->
             ( { model | page = SearchResult response.results }, Cmd.none )
 
@@ -127,12 +169,35 @@ update msg model =
             ( model, Cmd.none )
 
 
+updateAfochPage : Model -> String -> Model
+updateAfochPage model afocha =
+    case model.page of
+        DetailView deceased ->
+            { model | page = DetailView { deceased | afocha = afocha } }
+
+        _ ->
+            model
+
+
+fetchAfocha : String -> Cmd Msg
+fetchAfocha afochaUrl =
+    Http.get
+        { url = afochaUrl
+        , expect = Http.expectJson GotAfochaResponse afochaDecoder
+        }
+
+
 searchQebr : String -> Cmd Msg
 searchQebr query =
     Http.get
         { url = qebrUrl ++ "deceased/?search=" ++ query
         , expect = Http.expectJson GotQebrResponse qebrDecoder
         }
+
+
+afochaDecoder : Decoder String
+afochaDecoder =
+    field "name" string
 
 
 qebrDecoder : Json.Decode.Decoder QebrResponse
@@ -187,7 +252,7 @@ subscriptions _ =
 
 main : Program () Model Msg
 main =
-    Browser.element
+    Browser.document
         { init = init
         , view = view
         , update = update
