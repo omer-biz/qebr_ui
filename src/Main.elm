1f75f78779bc0ac0e86fa98f8909631701476c04
omer <omerabdi@protonmail.com>
2023-12-07 18:57:45 +0300

    fix: better rendering ????

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,10 @@
 module Main exposing (main)
 
 import Browser
-import Element exposing (alignBottom, alignRight, centerX, fill, padding, spacing)
+import Element exposing (alignBottom, alignRight, centerX, fill, mouseDown, mouseOver, padding, rgb255, spacing)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 import Element.Input as Input
 import Http
 import Json.Decode exposing (Decoder, fail, field, float, int, list, nullable, string, succeed)
@@ -74,6 +77,12 @@ type alias QebrResponse =
     }
 
 
+
+-- sampleDeceased : Deceased
+-- sampleDeceased =
+--     Deceased "Ali Ahmed Abdi" 134 Male 23453 "Haji Ali" 2 <| Location 12.1 221
+
+
 type Msg
     = SearchField String
     | Search
@@ -100,14 +109,37 @@ view model =
 viewHeader : Element.Element msg
 viewHeader =
     let
-        textAttribute =
-            [ spacing 10, alignRight, padding 10 ]
+        boxShadow =
+            Border.shadow
+                { blur = 5
+                , color = rgb255 0x00 0x00 0x00
+                , offset = ( 1, 1 )
+                , size = 0.5
+                }
     in
-    Element.row textAttribute
-        [ Element.text "About Us"
-        , Element.text "Contact Us"
-        , Element.text "Change Language"
+    Element.el
+        [ Element.width fill
+        , boxShadow
         ]
+    <|
+        Element.row
+            [ spacing 10
+            , padding 10
+            , alignRight
+            ]
+            [ headerLink "About Us"
+            , headerLink "Contact Us"
+            ]
+
+
+headerLink : String -> Element.Element msg
+headerLink content =
+    let
+        headerStyle =
+            [ Font.color <| rgb255 0x00 0x00 0x1F
+            ]
+    in
+    Element.link headerStyle { url = "https://www.google.com", label = Element.text content }
 
 
 viewFooter : Element.Element msg
@@ -117,9 +149,11 @@ viewFooter =
 
 viewContent : Model -> Element.Element Msg
 viewContent model =
-    Element.column [ Element.width fill ]
+    Element.column [ centerX, spacing 10 ]
         [ viewSearchField model.serchQuery
         , viewResults model.page
+
+        -- , viewDeceased sampleDeceased
         ]
 
 
@@ -138,31 +172,49 @@ viewResults page =
 
 viewListDeceased : List Deceased -> Element.Element Msg
 viewListDeceased results =
-    Element.row [ centerX ] <| List.map viewDeceased results
+    Element.row [ centerX, spacing 10 ] <| List.map viewDeceased results
 
 
 viewSearchField : String -> Element.Element Msg
 viewSearchField searchQuery =
     Element.column [ centerX ]
         [ Element.image [ Element.width (Element.px 400) ] { src = qebrUrl ++ logoPath, description = "Qebr" }
-        , Element.row []
-            [ Input.search [ Element.width Element.fill ]
+        , Element.row [ spacing 20 ]
+            [ Input.search [ padding 5, Border.width 2 ]
                 { onChange = SearchField
                 , text = searchQuery
                 , placeholder = Nothing
-                , label = Input.labelHidden ""
+                , label = Input.labelHidden "search label"
                 }
-            , Input.button [] { onPress = Just Search, label = Element.text "Search" }
+            , Input.button
+                simpleButtonStyle
+                { onPress = Just Search, label = Element.text "Search" }
             ]
         ]
 
 
+simpleButtonStyle : List (Element.Attribute msg)
+simpleButtonStyle =
+    [ mouseDown [ Background.color <| rgb255 182 86 139, Font.color <| rgb255 0xFF 0x00 0xFF ]
+    , mouseOver [ Background.color <| rgb255 86 182 139, Font.color <| rgb255 0xFF 0xFF 0xFF ]
+    , Border.rounded 5
+    , Border.width 1
+    , padding 5
+    ]
+
+
 viewDeceased : Deceased -> Element.Element Msg
 viewDeceased deceased =
-    Element.column [ Element.padding 10 ]
+    Element.column
+        [ Element.padding 10
+        , Border.color <| rgb255 0xFF 0x00 0x00
+        , Border.width 1
+        , spacing 5
+        , Border.rounded 2
+        ]
         [ Element.text deceased.fullName
         , Element.text <| String.fromInt deceased.qebele
-        , Input.button [] { onPress = Just (ViewDetailDeceased deceased), label = Element.text "More" }
+        , Input.button simpleButtonStyle { onPress = Just (ViewDetailDeceased deceased), label = Element.text "More" }
         ]
 
 
